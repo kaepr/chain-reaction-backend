85a7b51f1f605b461a71ccb0345deca0f09798e4
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-05-25 22:29:50 +0530

    set redis

--- src/helpers/redis/redis_init.js
+++ src/helpers/redis/redis_init.js
@@ -1,10 +1,23 @@
 import redis from 'redis';
 import logger from '../../utils/logger';
 
+// in prod
 const client = redis.createClient({
+  url: process.env.REDIS_URL,
+  // port: 6379,
+  // host: '127.0.0.1',
+});
+
+// in dev
+/**
+ * 
+ * const client = redis.createClient({
   port: 6379,
   host: '127.0.0.1',
 });
+ * 
+ * 
+ */
 
 client.on('connect', () => {
   logger.info('Client connected to redis...');
